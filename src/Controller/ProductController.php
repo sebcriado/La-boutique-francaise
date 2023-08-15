f156ad6cd8d156abcff27bf089a995345d27e438
Criado Sébastien <dev@letgy.com>
2023-08-15 14:15:36 +0200

    ajout barre de filtre produits

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Classe\Search;
 use App\Entity\Product;
+use App\Form\SearchType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,12 +21,23 @@ class ProductController extends AbstractController
 
     }
     #[Route('/nos-produits', name: 'app_product')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $products = $this->entityManager->getRepository(Product::class)->findAll();
 
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
+        } else {
+            $products = $this->entityManager->getRepository(Product::class)->findAll();
+        }
+        
         return $this->render('product/index.html.twig', [
-            'products' => $products
+            'products' => $products,
+            'form' => $form->createView()
         ]);
     }
 
